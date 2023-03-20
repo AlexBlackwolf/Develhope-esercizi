1d0c65fff1efd5e53d6df5f9553c2e028bbfcbcf
AlexBlackwolf <alex_fortini@hotmail.com>
2023-03-20 02:05:14 +0100

    exercise 2

--- src/javabasics/_16/Main.java
+++ src/javabasics/_16/Main.java
@@ -25,7 +25,7 @@ public class Main {
         String weatherStatement = conversionDescription(celsius, fahrenheit);
         System.out.println(weatherStatement);
     }
-        // inter your function
+        // Enter your function
         // End
         public static String conversionDescription(double celsius, double fahrenheit) {
             return celsius + " Celsius is " + fahrenheit + " Fahrenheit";
@@ -62,7 +62,7 @@ public class Main {
                 } else {
                     return trimmedName.toUpperCase();
                 }
-            }
+    }
 
     /**
      * 3: Write a function that prints out every variable seen in the code. It should not return anything
